b01e90efbc601a3ab2e0e3cd0b1591b95756a9bb
shilpi gupta <gupta.299shilpi@gmail.com>
2020-06-21 18:49:17 +0530

    Init Commit

---
+++ src/main/java/com/helloworld/demo/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.helloworld.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping
+	public String helloworld() {
+		return "HelloWorld!!!";
+	}
+	
+}
---
+++ src/main/java/com/helloworld/demo/HelloWorldDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldDockerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldDockerAppApplication.class, args);
+	}
+
+}
