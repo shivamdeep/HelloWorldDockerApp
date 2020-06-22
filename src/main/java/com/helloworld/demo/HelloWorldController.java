96f2de46b95dcbcdf5c5634bee7ca4c4920150d8
shilpi gupta <gupta.299shilpi@gmail.com>
2020-06-22 10:03:56 +0530

    Add Jenkins pipline config files

--- src/main/java/com/helloworld/demo/HelloWorldController.java
+++ src/main/java/com/helloworld/demo/HelloWorldController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-
 	@GetMapping
 	public String helloworld() {
 		return "HelloWorld!!!";
 	}
-	
 }
